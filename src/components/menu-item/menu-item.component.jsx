7979706d9e7dc6bc59f12c891f964c042d101bdc
Renzo Nahuel Murina Cadierno <nmcadierno@hotmail.com>
2020-02-26 18:15:35 -0300

    Updated App.js, added collection-preview and collection-item components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -10,8 +10,7 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     <div 
       className='background-image' 
       style={{ backgroundImage: `url(${ imageUrl })` }}>
-
-      </div>
+    </div>
     <div className='content'>
       <h1 className='title'> { title.toUpperCase() } </h1>
       <span className='subtitle'> Shop now</span>
