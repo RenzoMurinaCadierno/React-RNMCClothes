3df065356a90dd5b734cb27459818bfc7fd9e584
Renzo Nahuel Murina Cadierno <nmcadierno@hotmail.com>
2020-03-02 21:07:18 -0300

    Added cart icon and dropdown components, as well as cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { auth } from '../../firebase/firebase.utils'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 import './header.styles.scss'
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -22,13 +24,24 @@ const Header = ({ currentUser }) => (
         :
         <Link className='option' to='/signin'> SIGN IN </Link>
       }
+      <CartIcon />
     </div>
+    {
+      // hidden from redux state below is false? Render.
+      hidden ? null : <CartDropdown />
+    }
   </div>
 )
 
-// the state we get here is the root reducer
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+// the state we are passed here is the root reducer (state)
+// we know that state holds user object, and that user object
+// has currentUser inside. Same goes for cart: hidden state.
+// So we can nest destructure those two properties like this:
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  
+  // and instead of doing currentUser: currentUser, ES6, do one.
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps)(Header)
