aac3f70ec12c6929ec3a75d26cdfbd8dfc47804a
Renzo Nahuel Murina Cadierno <nmcadierno@hotmail.com>
2020-03-04 19:33:16 -0300

    Added addItem and removeItem functionality to checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,16 +1,52 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions'
 import './checkout-item.styles.scss'
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={ imageUrl } alt="item"/>
-    </div>
-    <span className='name'> { name } </span>
-    <span className='quantity'> { quantity } </span>
-    <span className='price'> ${ price } </span>
-    <span className='remove'> &#10005; </span>
-  </div>
-)
+// we need the whole cartItem object to pass it as dispatch to
+// clearItem, so, we destructure the rest inside CheckoutItem.
+// Otherwise, we would not have access to the whole object if
+// we nested destructured instead.
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
 
-export default CheckoutItem
+  const { name, imageUrl, price, quantity } = cartItem
+
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={ imageUrl } alt="item"/>
+      </div>
+      <span className='name'> { name } </span>
+      <span className='quantity'> 
+        <span 
+          className='arrow' 
+          onClick={ () => removeItem(cartItem)}
+        > 
+          &#10094; 
+        </span>
+        <span className='value'> { quantity } </span> 
+        <span 
+          className='arrow'
+          onClick={ () => addItem(cartItem) }
+        > 
+          &#10095; 
+        </span>
+      </span>
+      <span className='price'> ${ price } </span>
+      <span 
+        className='remove' 
+        onClick={ () => clearItem(cartItem)}
+      > 
+        &#10005; 
+      </span>
+    </div>
+  )
+}
+
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CheckoutItem)
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -24,3 +24,27 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   // quantity == 1
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 }
+
+// decrease the items from the checkout
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+
+  // get the item being decreased from the state array of cartItems
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  )
+
+  // are we decreasing the last item to 0?
+  if (existingCartItem.quantity === 1) {
+
+    // then filter the array and remove that item by its id
+    return cartItems.filter( cartItem => cartItem.id !== cartItemToRemove.id)
+  }
+
+  return cartItems.map( cartItem =>
+    cartItem.id === cartItemToRemove.id 
+    ?
+      { ...cartItem, quantity: cartItem.quantity - 1 }
+    :
+      cartItem
+  )
+}
