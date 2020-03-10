cb55085d7d03380cf0646b593f8f314c75b75339
Renzo Nahuel Murina Cadierno <nmcadierno@hotmail.com>
2020-03-09 21:50:52 -0300

    Loaded shop items collections to firebase, updated shop reducer to handle the dynamic data now coming from firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,13 +12,22 @@ const config = {
   appId: "1:1016116059704:web:20d39e644981be4c5e90cd"
 }
 
+firebase.initializeApp(config)
+
 export const createUserProfileDocument = async (userAuthObj, additionalDataObj) => {
   
   // if there is no user object (not logged in), nothing to do
   if (!userAuthObj) return
 
+  // get the user reference object with its UID stored inside 
+  // the user object. The reference is the path to that user
+  // object and the UID. It is NOT the snapshot! The snapshot
+  // is the actual data that user document holds.
   const userRef = firestore.doc(`users/${ userAuthObj.uid }`)
 
+  // once we got the reference to that document, get its snapshot.
+  // The reference contains info on how to access it, the snapshot
+  // has all the docs/collections it stores
   const snapshot = await userRef.get()
 
   // if the snapshot does not exist, the user does not either.
@@ -31,6 +40,9 @@ export const createUserProfileDocument = async (userAuthObj, additionalDataObj) 
     // store the user creation date 
     const createdAt = new Date()
 
+    // use all of that data to create the user snapshot, and set
+    // it to its respective user reference (which holds the path)
+    // to it.
     try {
       await userRef.set({
         displayName,
@@ -47,7 +59,74 @@ export const createUserProfileDocument = async (userAuthObj, additionalDataObj) 
   return userRef
 }
 
-firebase.initializeApp(config)
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+
+  // get the target collection reference using its key
+  const collectionRef = firestore.collection(collectionKey)
+
+  // create a batch object for the batch call
+  const batch = firestore.batch()
+
+  // loop over the objectsToAdd (array), to add it to the batch
+  objectsToAdd.forEach( obj => {
+
+    // ask Firebase to give you a new empty docRef with a unique key.
+    // This action will create 5 documents, one for each shop item
+    // (hats, sneakers, jackets, men, women), each one empty with an id.
+    const newDocRef = collectionRef.doc()
+
+    // set each item object to this empty doc, in the batch
+    batch.set(newDocRef, obj)
+  })
+
+  // fire off the request. It returns a Promise with either a void
+  // (null) value in success. We can chain with .then each time we
+  // want to send requests for it, or handle errors
+  return await batch.commit()
+}
+
+export const convertCollectionsSnapshotToMap = collections => {
+
+  // get the array of QuerySnapshots from onSnapshot() in
+  // shop.component, and pap them one by one
+  const transformedCollections = collections.docs.map( doc => {
+
+    // for each of those returned documents, use data() to convert
+    // them to JS objects, and desctructure the title and items
+    const { title, items } = doc.data()
+
+    // create and return the final shop collection object to render
+    // in our frontend
+    return {
+
+      // encodeURI is a method that comes with every JS renderer.
+      // It converts any non-readable characters coming from the 
+      // URI into a version the browser can read
+      routeName: encodeURI(title.toLowerCase()),
+
+      // the ID will be the one in firebase's snapShot
+      id: doc.id,
+
+      // title and items are as we destructured them
+      title,
+      items
+    }
+  })
+
+  // reduce the generated array to an object whose keys are the
+  // collection names and values, the collection object. Exactly
+  // like what we had in the big object of collection items:
+  // { hats: { hats collection }, jackets: { jackets collection }, ... }
+  // Make sure to return the accumulator to enter in the next 
+  // iteration, and to return this newly generated object.
+  return transformedCollections.reduce( (accumulator, collection) => {
+    
+    accumulator[collection.title.toLowerCase()] = collection
+    
+    return accumulator
+  
+  }, {} )   // start from an empty object
+}
 
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,66 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { updateCollections } from '../../redux/shop/shop.actions'
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 import CollectionPage from '../collection/collection.component'
 
-const ShopPage = ({ match }) => (
+class ShopPage extends React.Component {
 
-  <div className='shop-page'>
-    <Route 
-      exact path={ `${ match.path }` }  // shop/ wherever it is
-      component={ CollectionsOverview } // which makes comp reusable! 
-    />
-    <Route 
-      path={ `${match.path}/:collectionId`} 
-      component={ CollectionPage }
-    />
-  </div>
-)
+  // set the unsubscribe variable we will use when unmounting
+  unsubscribeFromSnapshot = null
 
-export default ShopPage
+  componentDidMount() {
+
+    // destructure updateCollections action from props coming 
+    // from redux below
+    const { updateCollections } = this.props 
+
+    // bring the 'collections' collection reference from firestore
+    const collectionRef = firestore.collection('collections')
+  
+    // whenever the reference updates or when the component mounts,
+    // onSnapshot() will send us a snapshot from it. Since the object
+    // we need to use is inside this snapshot, then we need to await
+    // for the answer before proceeding.
+    collectionRef.onSnapshot( async snapshot => {
+
+      // from the snapshot, convert each QuerySnapshot in the array
+      // in docs to the objects we need to render. Explanation in
+      // firebase.utils.js
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+
+      // dispatch the action on the returned collectionMap (collKey:
+      // coll obj), to update it with new values
+      updateCollections(collectionsMap)
+    })
+  }
+
+  render() {
+
+    // get the match from the received props
+    const { match } = this.props
+
+    return (
+      <div className='shop-page'>
+        <Route 
+          exact path={ `${ match.path }` }  // shop/ wherever it is
+          component={ CollectionsOverview } // which makes comp reusable! 
+        />
+        <Route 
+          path={ `${match.path}/:collectionId`} 
+          component={ CollectionPage }
+        />
+      </div>
+    )
+  }
+} 
+
+const mapDispatchToProps = dispatch => ({
+
+  updateCollections: collectionsMap => 
+    dispatch(updateCollections(collectionsMap))
+})
+
+export default connect(null, mapDispatchToProps)(ShopPage)
