f8e8557c7a26f467e6df917cca1e446af263cfe5
Renzo Nahuel Murina Cadierno <nmcadierno@hotmail.com>
2020-03-27 23:55:32 -0300

    Removed contact link

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -17,7 +17,6 @@ const Header = ({ currentUser, hidden, signoutStart }) => (
     </LogoContainer>
     <OptionsContainer>
       <OptionLink to='/shop'> SHOP </OptionLink>
-      <OptionLink to='/'> CONTACT </OptionLink>
       {
         currentUser ?
         <OptionLink as='div' onClick={ signoutStart }>
